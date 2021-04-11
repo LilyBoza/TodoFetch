881314a0db058c012b545b4f24210ad23ab0b026
LilyBoza <lily96boza@gmail.com>
2021-04-11 13:33:21 +0000

    primerTodo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,83 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 export function Home() {
+	const FetchUrl =
+		"https://assets.breatheco.de/apis/fake/todos/user/usuarioLilianaPBN";
+	const [todos, setTodos] = useState([]);
+
+	const addTodo = async e => {
+		if (e.key == "Enter") {
+			if (e.target === "") {
+				alert("ingrese la tarea");
+			} else {
+				const NuevoTodo = { label: e.target.value, done: false };
+				await fetch(FetchUrl, {
+					method: "PUT",
+					body: JSON.stringify([...todos, NuevoTodo]),
+					headers: { "Content-Type": "application/json" }
+				})
+					.then(res => res.json())
+					.then(data => console.log([data]));
+
+				await fetch(FetchUrl)
+					.then(res => res.json())
+					.then(data => {
+						setTodos(data);
+						e.target.value = " ";
+					});
+			}
+		}
+	};
+
+	//Función de eliminar todo sin API
+	// const delTodo = key => {
+	// 	setTodos(
+	// 		todos.filter((item, index) => {
+	// 			return key !== index ? item : null;
+	// 		})
+	// 	);
+	// };
+
+	const delTodo = async key => {
+		if (todos.length === 1) {
+			setTodos([]);
+		} else {
+			const listaNueva = todos.filter((item, index) => {
+				return key !== index ? item : null;
+			});
+
+			await fetch(FetchUrl, {
+				method: "PUT",
+				body: JSON.stringify(listaNueva),
+				headers: { "Content-Type": "application/json" }
+			})
+				.then(res => res.json())
+				.then(data => console.log([data]));
+
+			await fetch(FetchUrl)
+				.then(res => res.json())
+				.then(data => {
+					setTodos(data);
+					// e.target.value = " ";
+				});
+		}
+	};
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>TodoList</h1>
+			<input onKeyDown={addTodo} />
+			<ul>
+				{todos.map((todo, key) => {
+					return (
+						<li key={key}>
+							{todo.label}
+							<button onClick={() => delTodo(key)}>x</button>
+						</li>
+					);
+				})}
+			</ul>
 		</div>
 	);
 }
